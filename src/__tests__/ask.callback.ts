90debc1147490015542202a5449994ff5c6ddd53
Attila Gal <attila.gal@gmail.com>
2018-06-14 12:57:07 +0200

    split up tests

--- src/__tests__/ask.callback.ts
+++ src/__tests__/ask.callback.ts
@@ -87,36 +87,4 @@ describe('riddler', () => {
       expect(messages[6]).toBe('2) quit');
     });
   });
-
-  describe('riddler#askResponse()', () => {
-    let messages = [];
-    let receivedFooter;
-    let triggerCallback;
-    beforeEach(() => {
-      console.log = jest.fn(log => messages.push(log));
-      mockReadline.createInterface = jest.fn(() => ({
-        question: (givenFooter, callback) => {
-          receivedFooter = givenFooter;
-          triggerCallback = callback;
-        },
-        close: () => 'closing'
-      }));
-      todd.askResponse({
-        question: 'What\'s your favourite colour?',
-        callback: (err, res) => messages.push(res)
-      });
-    });
-    afterEach(() => {
-      messages = [];
-    });
-
-    it('should ask the question first', () => {
-      expect(messages[0]).toBe('What\'s your favourite colour?');
-    });
-
-    it('should return the response when user response was received', () => {
-      triggerCallback('my answer');
-      expect(messages[1]).toBe('my answer');
-    });
-  });
 });
